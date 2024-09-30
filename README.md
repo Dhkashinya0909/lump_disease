eb7f5218317e087a978ddcf56c5a20e28687b7b7
Dhkashinya0909 <dakshu6938@gmail.com>
2024-09-30 21:42:49 +0530

    updated model_inference according with mac os compactibliity

--- README.md
+++ README.md
@@ -9,3 +9,24 @@
 # Weights files will be found in weights folder
 - Download DenseNet weights at https://www.kaggle.com/datasets/xhlulu/densenet-keras
 - Download Resnet weights at https://www.kaggle.com/datasets/bharatsingh213/resnet50weightsfile
+
+
+
+
+# Mac os Setup
+- Create a conda environment
+```conda create --profile ./env```
+- Activate the Environment
+```conda activate ./env```
+- install tensorflow dependencies for macos
+```conda install -c apple tensorflow-deps```
+- install all the reqirements
+```python3 -m pip install -r reqirements_mac.txt```
+
+# Linux Os Setup
+- Create a conda environment
+```conda create --profile ./env```
+- Activate the Environment
+```conda activate ./env```
+- install all the reqirements
+```python3 -m pip install -r reqirements1.txt```
--- model_inference.py
+++ model_inference.py
@@ -1,60 +1,71 @@
 # Importing Libraries
 from tensorflow import keras
-from tensorflow.keras.applications.resnet50 import ResNet50
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Flatten, MaxPooling2D, Dense, Dropout, GlobalAveragePooling2D
-from tensorflow.keras import optimizers, losses
-from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.preprocessing import image
-import cv2
-import pickle
 import numpy as np
 import matplotlib.pyplot as plt
-
 import warnings
+import os
+
+# Ignore warnings
 warnings.filterwarnings("ignore")
 
-# Check our folder and import the model with best validation accuracy
+# Ensure Matplotlib is using 'Agg' backend to avoid macOS GUI errors
+import matplotlib
+matplotlib.use('Agg')  # This disables GUI rendering on macOS
+
+# Optional: Disable GPU usage if not needed
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+# Load the model with the best validation accuracy
 loaded_best_model = keras.models.load_model("./model_07-0.78.h5")
 
 # Custom function to load and predict label for the image
 def predict(img_rel_path):
+    # Load the image and resize to 256x256
     img = image.load_img(img_rel_path, target_size=(256, 256))
-
+    
     # Convert Image to a numpy array
     img = image.img_to_array(img, dtype=np.uint8)
-
-    # Scaling the Image Array values between 0 and 1
-    img = np.array(img)/255.0
-
+    
+    # Scale the Image Array values between 0 and 1
+    img = np.array(img) / 255.0
+    
     # Get the Predicted Label for the loaded Image
     p = loaded_best_model.predict(img[np.newaxis, ...])
-
+    
     # Label array
     labels = {0: 'Lumpy Skin', 1: 'Normal Skin'}
-
+    
+    # Find the maximum probability and predicted class
     maximum_probability = np.max(p[0], axis=-1)
     predicted_class = labels[np.argmax(p[0], axis=-1)]
-
-    classes=[]
-    prob=[]
-    for i,j in enumerate (p[0],0):
-        print(labels[i].upper(),':',round(j*100,2),'%')
+    
+    # Prepare data for the bar plot
+    classes = []
+    prob = []
+    
+    for i, j in enumerate(p[0], 0):
+        print(labels[i].upper(), ':', round(j * 100, 2), '%')
         classes.append(labels[i])
-        prob.append(round(j*100,2))
-        
+        prob.append(round(j * 100, 2))
+    
+    # Function to generate the probability bar plot
     def plot_bar_x():
-        # this is for plotting purpose
+        # This is for plotting purpose
         index = np.arange(len(classes))
         plt.bar(index, prob)
         plt.xlabel('Labels', fontsize=8)
         plt.ylabel('Probability', fontsize=8)
         plt.xticks(index, classes, fontsize=8, rotation=20)
         plt.title('Probability for loaded image')
-        file_path = f"./static/graphs/{img_rel_path.split('/')[-1]}"
-        plt.savefig(file_path)  # Saving the plot instead of showing it
-        plt.close()  # Close the plot to free resources
+        
+        # Save the plot as an image
+        file_path = f"./static/graphs/{os.path.basename(img_rel_path)}"
+        plt.savefig(file_path)
+        plt.close()  # Make sure to close the plot to avoid resource leaks
+        
         return file_path
+    
+    # Generate and return the plot file path along with the predictions
     file_path = plot_bar_x()
-    return (maximum_probability,predicted_class,file_path)
+    return (maximum_probability, predicted_class, file_path)
